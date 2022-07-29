366c2d861a134f348ed9376369645d76813322b1
Rohan Kumar <rohankumar25032004@gmail.com>
2022-07-29 15:51:11 +0530

    Node JS Modules

---
+++ 04_Node_JS/02_NodeJS_Modules.js
@@ -0,0 +1,13 @@
+// const fs = require('fs');
+// let text = fs.readFileSync("Extra.txt", "utf-8");
+// text = text.replace("browser", "Rohan");
+
+// console.log("The content of the file is")
+// console.log(text);
+
+// console.log("Creating a new file...")
+// fs.writeFileSync("rohan.txt", text);
+
+const fs = require('fs');
+let text = fs.readFileSync("Extra.txt", "utf-8");
+console.log(text);
